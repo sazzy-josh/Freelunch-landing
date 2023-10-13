08ffa8eb73666b1bb574e4a833e2ca2017836421
Idahosa Joshua <idahosajoshua61@gmail.com>
2023-10-13 16:54:48 +0100

    initials

---
+++ src/pages/layout.tsx
@@ -0,0 +1,34 @@
+import {Poppins} from "next/font/google";
+import localFont from "@next/font/local";
+
+export const poppins = Poppins({
+  weight: ["400", "500", "600", "700"],
+  style: ["normal", "italic"],
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-poppins",
+});
+
+const stapel = localFont({
+  src: [
+    {
+      path: "/fonts/Stapel-ExpandedBlack.ttf",
+      weight: "900",
+    },
+    {
+      path: "/fonts/Stapel-SemiExpandedExtraBold.ttf",
+      weight: "700",
+    },
+  ],
+  variable: "--font-stapel",
+});
+
+const RootLayout = ({children}: {children: React.ReactNode}) => {
+  return (
+    <main className={`${poppins.variable}  ${stapel.variable}`}>
+      {children}
+    </main>
+  );
+};
+
+export default RootLayout;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,34 @@
-import type { Config } from 'tailwindcss'
+import type {Config} from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      fontFamily: {
+        pop: ["var(--poppins)"],
+        stapel: ["var(--font-stapel)"],
       },
     },
   },
-  plugins: [],
-}
-export default config
+  plugins: [
+    function ({addUtilities}: any) {
+      const newUtilities = {
+        ".font-young-serif": {
+          fontFamily: "Young Serif, serif",
+        },
+      };
+
+      addUtilities(newUtilities, ["responsive", "hover"]);
+    },
+  ],
+};
+export default config;
